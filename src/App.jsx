02dc911996c9cbb42cf97e434aa31ec6d1992b6d
Waleed Shahid <WaleedShahid837@gmail.com>
2025-09-29 21:30:47 +0500

    commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import HeroSection from "./components/HeroSection";
+import HeroCardImg from "./components/HeroCardImg";
+import Products from "./components/Products";
+import CartButton from "./components/CartButton";
+import { Box } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />}></Route>
+            <Route path="offer" element={<h1>Offer</h1>}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
