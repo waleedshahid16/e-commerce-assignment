3e47f260c373989d79c7092bb371b123e574c95b
Waleed Shahid <waleedshahid838@gmail.com>
2025-10-08 02:38:10 +0500

    Commit this  files with original code

--- src/components/style.js
+++ src/components/style.js
@@ -1,34 +1,17 @@
-<<<<<<< HEAD
 export const navButtonStyle = {
   textTransform: "none",
   color: "black",
   px: 1.5,
   "&:hover": { color: "rgb(55 65 81)" },
 };
+
 export const ProductDetailModalStyle = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 1000,
   bgcolor: "white",
   boxShadow: 24,
   p: 4,
-=======
-export const navButtonStyle = {
-  textTransform: "none",
-  color: "black",
-  px: 1.5,
-  "&:hover": { color: "rgb(55 65 81)" },
-};
-export const ProductDetailModalStyle = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 1000,
-  bgcolor: "white",
-  boxShadow: 24,
-  p: 4,
->>>>>>> b6573e3 (Added updated project files)
 };
